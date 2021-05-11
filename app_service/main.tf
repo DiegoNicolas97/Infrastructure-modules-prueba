1a73e614494af44c4820461c609eb212755b2cd8
drojas93 <drojas93@summa-sci.com>
2021-05-11 10:22:22 -0400

    Se removieron las variables de imagen del appservi

--- app_service/main.tf
+++ app_service/main.tf
@@ -4,33 +4,8 @@ terraform {
 
 }
 
-resource "azurerm_app_service" "container" {
+resource "azurerm_app_service" "app_service_terraform" {
 
-  count               = var.image == "" ? 0 : 1
-  name                = var.app_service_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  app_service_plan_id = var.service_plan_id
-
-  site_config {
-
-    linux_fx_version = var.image
-    always_on        = "true"
-
-  }
-
-  app_settings = {
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-    "DOCKER_REGISTRY_SERVER_URL"          = var.docker_container_registry_url
-    "DOCKER_ENABLE_CI"                    = "false"
-    "DOCKER_REGISTRY_SERVER_USERNAME"     = var.docker_container_registry_username
-    "DOCKER_REGISTRY_SERVER_PASSWORD"     = var.docker_container_registry_password
-  }
-}
-
-resource "azurerm_app_service" "normal" {
-
-  count               = var.image == "" ? 1 : 0
   name                = var.app_service_name
   location            = var.location
   resource_group_name = var.resource_group_name
--- app_service/variables.tf
+++ app_service/variables.tf
@@ -14,18 +14,3 @@ variable "service_plan_id" {
   description = "Id of the app service plan"
 }
 
-variable "image" {
-  description = " Imagen de docker"
-}
-
-variable "docker_container_registry_url" {
-  description = "URL of the container registry"
-}
-
-variable "docker_container_registry_username" {
-  description = "Username of the container registry"
-}
-
-variable "docker_container_registry_password" {
-  description = "Password of the container registry"
-}
