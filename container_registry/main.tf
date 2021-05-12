41733958606dbb41545df396b5cad039052251a8
drojas93 <drojas93@summa-sci.com>
2021-05-12 09:13:24 -0400

    Se agrego container registry

---
+++ container_registry/main.tf
@@ -0,0 +1,12 @@
+terraform {
+    backend "azurerm" {}
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                     = var.container_registry_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  sku                      = var.container_registry_sku
+  admin_enabled            = false
+  #georeplication_locations = ["East US", "West Europe"]
+}
---
+++ container_registry/variables.tf
@@ -0,0 +1,15 @@
+variable container_registry_name {
+    description = "Name of the container registry"
+}
+
+variable location {
+    description = "Location of the container registry"
+}
+
+variable resource_group_name {
+    description = "Resource Group Name for the container registry"
+}
+
+variable container_registry_sku {
+    description = "sku of container registry"
+}
